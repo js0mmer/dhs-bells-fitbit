6d628c72173f0f2b7c6aadee8114e3b4ac5bf93e
Jacob Sommer <jacob.sommer@comcast.net>
2018-12-10 17:46:29 -0800

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,128 @@
+/*
+ * Entry point for the watch app
+ */
+import document from "document";
+import { readFileSync } from "fs";
+
+var date = new Date();
+var period = document.getElementById("period");
+var ending = document.getElementById("ending");
+var nextPeriod = document.getElementById("nextPeriod");
+var nextPeriodTime = document.getElementById("nextPeriodTime");
+
+function init() {
+  var day = date.getDay();
+
+  if(day != 0 && day != 6) {
+    var file = 'normal';
+    
+    if(day == 3) {
+      file = 'wednesday';
+    }
+    
+    // Finals
+    if(date.getMonth() == 4) {
+      if(date.getDate() == 25) {
+        file = 'finals/friday';
+      } else if(date.getDate() == 29) {
+        file = 'finals/tuesday';
+      } else if(date.getDate() == 30) {
+        file = 'finals/wednesday';
+      } else if(date.getDate() == 31) {
+        file = 'finals/thursday';
+      }
+    } else if (date.getMonth() == 10 && date.getDate() == 2) {
+      file = 'rally.json';
+    }
+    
+    getPeriod(readFileSync("resources/schedules/" + file + ".json", "json"));
+  } else {
+    period.text = "No Class";
+  }
+}
+
+function inPeriod(periods, i, time) {
+  return time >= parseRawTime(periods[i].start) && time < parseRawTime(periods[i].end);
+}
+
+function inPassingPeriod(periods, i, time) {
+  return time >= parseRawTime(periods[i].end) && i < periods.length - 1 && time < parseRawTime(periods[i + 1].start);
+}
+
+function beforeSchool(periods, time) {
+  return time < parseRawTime(periods[0].start);
+}
+
+function getPeriod(s) {
+  var time = rawTime();
+  
+  if(beforeSchool(s.periods, time)) {
+      period.text = "No Class";
+      nextPeriod.text = "Next: " + s.periods[0].name;
+      nextPeriodTime.text = convertTo12Hour(s.periods[0].start) + " - " + convertTo12Hour(s.periods[0].end);
+      return;
+  }
+
+  for(var i = 0; i < s.periods.length; i++) {
+    if(inPeriod(s.periods, i, time)) {
+      period.text = s.periods[i].name;
+      ending.text = "Ends at " + convertTo12Hour(s.periods[i].end);
+      if(i < s.periods.length - 1) {
+        nextPeriod.text = "Next: " + s.periods[i + 1].name;
+        nextPeriodTime.text = convertTo12Hour(s.periods[i + 1].start) + " - " + convertTo12Hour(s.periods[i + 1].end);
+      }
+      break;
+    } else if(inPassingPeriod(s.periods, i, time)) {
+      period.text =  "Passing Period";
+      ending.text = "Ends at " + convertTo12Hour(s.periods[i + 1].start);
+      nextPeriod.text = "Next: " + s.periods[i + 1].name;
+      nextPeriodTime.text = convertTo12Hour(s.periods[i + 1].start) + " - " + convertTo12Hour(s.periods[i + 1].end);
+      break;
+    } else {
+      period.text = "No Class";
+    }
+  }
+}
+
+function convertTo12Hour(t) {
+  if(t.length < 5) {
+    return t + " AM";
+  } else if(parseInt(t.substring(0, 2)) < 12) {
+    return t + " AM";
+  } else if(parseInt(t.substring(0, 2)) == 12) {
+    return t + " PM";
+  } else {
+    return (parseInt(t.substring(0, 2)) - 12) + t.substring(2, 5) + " PM";
+  }
+}
+
+function parseRawTime(t) {
+  return parseInt(t.replace(":", ""));
+}
+
+function rawTime() {
+  var minutes = date.getMinutes().toString();
+
+  if(minutes.length == 1) {
+    minutes = "0" + minutes;
+  }
+
+  return parseInt(date.getHours() + minutes);
+}
+
+function fancyTime() {
+  var hours = date.getHours();
+  var minutes = date.getMinutes().toString();
+
+  if(hours > 12) {
+    hours = hours - 12;
+  }
+
+  if(minutes.length == 1) {
+    minutes = "0" + minutes;
+  }
+
+  return hours + ":" + minutes + (date.getHours() > 12 ? " PM" : " AM");
+}
+
+init();
