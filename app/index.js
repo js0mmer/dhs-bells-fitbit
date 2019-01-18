21393512c970e2657789c2992e97b94e846af7be
Jacob Sommer <jacob.sommer@comcast.net>
2019-01-17 18:24:59 -0800

    Formatting + assembly schedule

--- app/index.js
+++ app/index.js
@@ -13,34 +13,36 @@ var nextPeriodTime = document.getElementById("nextPeriodTime");
 function init() {
   var day = date.getDay();
 
-  if(day != 0 && day != 6) {
+  if (day != 0 && day != 6) {
     var file = 'normal';
     
-    if(day == 3) {
+    if (day == 3) {
       file = 'wednesday';
     }
     
-     // Sem 1 Finals
-    if(date.getMonth() == 11) {
-      if(date.getDate() == 18) {
+    // Sem 1 Finals
+    if (date.getMonth() === 11) {
+      if (date.getDate() === 18) {
         file = 'finals1/tuesday';
-      } else if(date.getDate() == 19) {
+      } else if (date.getDate() === 19) {
         file = 'finals1/wednesday';
-      } else if(date.getDate() == 20) {
+      } else if (date.getDate() === 20) {
         file = 'finals1/thursday';
-      } else if(date.getDate() == 21) {
+      } else if (date.getDate() === 21) {
         file = 'finals1/friday';
       }
-    } else if(date.getMonth() == 4) { // Sem 2 Finals
-      if(date.getDate() == 25) {
+    } else if (date.getMonth() === 4) { // Sem 2 Finals
+      if (date.getDate() === 25) {
         file = 'finals2/friday';
-      } else if(date.getDate() == 29) {
+      } else if (date.getDate() === 29) {
         file = 'finals2/tuesday';
-      } else if(date.getDate() == 30) {
+      } else if (date.getDate() === 30) {
         file = 'finals2/wednesday';
-      } else if(date.getDate() == 31) {
+      } else if (date.getDate() === 31) {
         file = 'finals2/thursday';
       }
+    } else if (date.getMonth() === 0 && date.getDate() === 18) { // Assembly
+      file = 'assembly';
     }
     
     getPeriod(readFileSync("resources/schedules/" + file + ".json", "json"));
@@ -64,27 +66,31 @@ function beforeSchool(periods, time) {
 function getPeriod(s) {
   var time = rawTime();
   
-  if(beforeSchool(s.periods, time)) {
-      period.text = "No Class";
-      nextPeriod.text = "Next: " + s.periods[0].name;
-      nextPeriodTime.text = convertTo12Hour(s.periods[0].start) + " - " + convertTo12Hour(s.periods[0].end);
-      return;
+  if (beforeSchool(s.periods, time)) {
+    period.text = "No Class";
+    nextPeriod.text = "Next: " + s.periods[0].name;
+    nextPeriodTime.text = convertTo12Hour(s.periods[0].start) + " - " + convertTo12Hour(s.periods[0].end);
+    
+    return;
   }
 
-  for(var i = 0; i < s.periods.length; i++) {
-    if(inPeriod(s.periods, i, time)) {
+  for (var i = 0; i < s.periods.length; i++) {
+    if (inPeriod(s.periods, i, time)) {
       period.text = s.periods[i].name;
       ending.text = "Ends at " + convertTo12Hour(s.periods[i].end);
-      if(i < s.periods.length - 1) {
+      
+      if (i < s.periods.length - 1) {
         nextPeriod.text = "Next: " + s.periods[i + 1].name;
         nextPeriodTime.text = convertTo12Hour(s.periods[i + 1].start) + " - " + convertTo12Hour(s.periods[i + 1].end);
       }
+      
       break;
     } else if(inPassingPeriod(s.periods, i, time)) {
       period.text =  "Passing Period";
       ending.text = "Ends at " + convertTo12Hour(s.periods[i + 1].start);
       nextPeriod.text = "Next: " + s.periods[i + 1].name;
       nextPeriodTime.text = convertTo12Hour(s.periods[i + 1].start) + " - " + convertTo12Hour(s.periods[i + 1].end);
+      
       break;
     } else {
       period.text = "No Class";
@@ -93,11 +99,11 @@ function getPeriod(s) {
 }
 
 function convertTo12Hour(t) {
-  if(t.length < 5) {
+  if (t.length < 5) {
     return t + " AM";
-  } else if(parseInt(t.substring(0, 2)) < 12) {
+  } else if (parseInt(t.substring(0, 2)) < 12) {
     return t + " AM";
-  } else if(parseInt(t.substring(0, 2)) == 12) {
+  } else if (parseInt(t.substring(0, 2)) === 12) {
     return t + " PM";
   } else {
     return (parseInt(t.substring(0, 2)) - 12) + t.substring(2, 5) + " PM";
@@ -111,26 +117,9 @@ function parseRawTime(t) {
 function rawTime() {
   var minutes = date.getMinutes().toString();
 
-  if(minutes.length == 1) {
-    minutes = "0" + minutes;
-  }
+  if (minutes.length == 1) minutes = "0" + minutes;
 
   return parseInt(date.getHours() + minutes);
-}
-
-function fancyTime() {
-  var hours = date.getHours();
-  var minutes = date.getMinutes().toString();
-
-  if(hours > 12) {
-    hours = hours - 12;
-  }
-
-  if(minutes.length == 1) {
-    minutes = "0" + minutes;
-  }
-
-  return hours + ":" + minutes + (date.getHours() > 12 ? " PM" : " AM");
 }
 
 init();
